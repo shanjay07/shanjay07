0899915b9f2ee3b037de32f05cef193445a9e1d9
shanjay07 <54099089+shanjay07@users.noreply.github.com>
2019-08-16 16:49:35 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util;
+public class largest
+{
+public static void main(String args[])
+{
+int n1,n2,n3;
+if(n1>=n2 && n1>=n3)
+System.out.println(n1 + "is the largest number");
+elseif(n2>=n1 && n2>=n3)
+System.out.println(n2 + "is the largest number");
+else
+System.out.println(n3 + "is the largest number");
+}
+}
